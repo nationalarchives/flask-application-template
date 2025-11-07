84bcb06e1650b84f39b7225b1a52dd5cd348039b
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-11-07 17:08:02 +0000

    Fix ENVIRONMENT_NAME assignment in Production class and reorder environment variables in docker-compose

--- config.py
+++ config.py
@@ -9,6 +9,7 @@ class Features:
 
 
 class Production(Features):
+    ENVIRONMENT_NAME: str = os.environ.get("ENVIRONMENT_NAME", "production")
     CONTAINER_IMAGE: str = os.environ.get("CONTAINER_IMAGE", "")
     BUILD_VERSION: str = os.environ.get("BUILD_VERSION", "")
     TNA_FRONTEND_VERSION: str = ""
