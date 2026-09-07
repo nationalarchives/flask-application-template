5b41485c2386a8d377304fb176ffde4ea45ef1bc
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2026-09-07 12:37:19 +0100

    Update dependencies, fix linting issues

--- app/lib/context_processor.py
+++ app/lib/context_processor.py
@@ -1,19 +1,18 @@
 import json
-from datetime import datetime
+from datetime import datetime, timezone
 from urllib.parse import unquote
 
 from flask import request
 
 
 def now_iso_8601():
-    now = datetime.now()
-    now_date = now.strftime("%Y-%m-%dT%H:%M:%SZ")
-    return now_date
+    now = datetime.now(tz=timezone.utc)
+    return now.strftime("%Y-%m-%dT%H:%M:%SZ")
 
 
 def cookie_preference(policy):
     if "cookie_preferences" in request.cookies:
         cookie_preferences = request.cookies["cookie_preferences"]
         preferences = json.loads(unquote(cookie_preferences))
-        return preferences[policy] if policy in preferences else None
+        return preferences.get(policy, None)
     return None
--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import json
 import os
+from typing import ClassVar
 
-from app.lib.util import strtobool
+from tna_utilities import strtobool
 
 
 class Features:
@@ -39,7 +40,7 @@ class Production(Features):
     CSP_REPORT_URI: str = os.environ.get("CSP_REPORT_URI", "")
     if CSP_REPORT_URI and BUILD_VERSION:
         CSP_REPORT_URI += f"&sentry_release={BUILD_VERSION}" if BUILD_VERSION else ""
-    CONTENT_SECURITY_POLICY: dict = {
+    CONTENT_SECURITY_POLICY: ClassVar[dict] = {
         "connect-src": os.environ.get("CSP_CONNECT_SRC", "").split(","),
         "font-src": os.environ.get("CSP_FONT_SRC", "").split(","),
         "frame-ancestors": os.environ.get("CSP_FRAME_ANCESTORS", "").split(","),
